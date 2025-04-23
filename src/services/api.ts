16c66219b66b411c0e189cc319195d62fca7b4d9
alex1431999 <alexhaller99@yahoo.de>
2025-04-23 13:08:40 +0200

    feat: add artifical delay to service

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,11 @@
+import {sleep} from "@/utils/sleep.ts";
+
 // TODO define once we know what the evaluation looks like
 export type PitchDeckEvaluation = any
 
 export class ApiService {
   public static async uploadPitchDeck(file: File): Promise<PitchDeckEvaluation> {
+    await sleep(3000);
     return {}
   }
 }
