8eff510ba6692e20b16f4a5aa71685efe93cb3fb
alex1431999 <alexhaller99@yahoo.de>
2025-04-23 12:55:55 +0200

    feat: add api service

---
+++ src/services/api.ts
@@ -0,0 +1,8 @@
+// TODO define once we know what the evaluation looks like
+export type PitchDeckEvaluation = any
+
+export class ApiService {
+  public static async uploadPitchDeck(file: File): Promise<PitchDeckEvaluation> {
+    return {}
+  }
+}
