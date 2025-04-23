ba1be601c9463fe77c772751b944c8ba2be8c1c9
alex1431999 <alexhaller99@yahoo.de>
2025-04-23 12:58:58 +0200

    feat: implement onUpload event

--- src/views/Evaluation.vue
+++ src/views/Evaluation.vue
@@ -1,14 +1,25 @@
 <script setup lang="ts">
 import UploadPitchDeck from '@/components/UploadPitchDeck.vue'
 import {ref} from "vue";
+import {ApiService, type PitchDeckEvaluation} from "@/services/api.ts";
 
 const pitchDeck = ref<File | null>(null)
+const pitchDeckEvaluation = ref<PitchDeckEvaluation | null>(null)
+const isLoading = ref(false)
+
+async function onUpload(file: File) {
+  pitchDeck.value = file
+
+  isLoading.value = true
+  pitchDeckEvaluation.value = await ApiService.uploadPitchDeck(file)
+  isLoading.value = false
+}
 </script>
 
 <template>
   <v-card width="1200px" height="600px">
     <v-card-text>
-      <UploadPitchDeck v-if="pitchDeck === null" @upload="file => pitchDeck = file" />
+      <UploadPitchDeck v-if="pitchDeck === null" @upload="onUpload" />
     </v-card-text>
   </v-card>
 </template>
