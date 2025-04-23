f6e6adb2be86b6a33d9359d1a4e6ffb8e94a0d8f
alex1431999 <alexhaller99@yahoo.de>
2025-04-23 16:49:38 +0200

    feat: add score component

--- src/components/EvaluationResults.vue
+++ src/components/EvaluationResults.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 
 import EvaluationSummary from "@/components/EvaluationSummary.vue";
+import EvaluationScore from "@/components/EvaluationScore.vue";
 </script>
 
 <template>
   <div class="d-flex justify-center">
     <EvaluationSummary class="mr-10" summary="Summary"/>
     <v-divider class="mr-10" vertical></v-divider>
-    <EvaluationSummary summary="Summary"/>
+    <EvaluationScore :score="{}"/>
   </div>
 </template>
---
+++ src/components/EvaluationScore.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const { score } = defineProps<{ score: any }>()
+</script>
+
+<template>
+  <v-card width="500" height="400" color="primary">
+    <v-card-title>Score</v-card-title>
+  </v-card>
+</template>
+
