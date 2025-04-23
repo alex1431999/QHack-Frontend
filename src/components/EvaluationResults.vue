79f5a29204d6aabf3d09335e752af09a7855a21a
alex1431999 <alexhaller99@yahoo.de>
2025-04-23 16:44:35 +0200

    feat: implement summary

--- src/components/EvaluationResults.vue
+++ src/components/EvaluationResults.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 
+import EvaluationSummary from "@/components/EvaluationSummary.vue";
 </script>
 
 <template>
-  Results
+  <div class="d-flex justify-center">
+    <EvaluationSummary class="mr-16" summary="Summary"/>
+    <EvaluationSummary summary="Summary"/>
+  </div>
 </template>
---
+++ src/components/EvaluationSummary.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const { summary } = defineProps<{ summary: string }>()
+</script>
+
+<template>
+  <v-card width="500" height="400" color="primary">
+    <v-card-title>Summary</v-card-title>
+    <v-card-text> {{ summary }}</v-card-text>
+  </v-card>
+</template>
+
