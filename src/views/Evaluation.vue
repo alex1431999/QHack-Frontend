484eff16dd043b540bcc2757c4be8e9a9966af2b
alex1431999 <alexhaller99@yahoo.de>
2025-04-23 12:28:20 +0200

    feat: add basic strucutre

--- src/views/Evaluation.vue
+++ src/views/Evaluation.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
-
+import UploadPitchDeck from '@/components/UploadPitchDeck.vue'
 </script>
 
 <template>
-  EVALUATION
+  <v-card>
+    <v-card-title>Evaluation</v-card-title>
+    <v-card-text>
+      <UploadPitchDeck />
+    </v-card-text>
+  </v-card>
 </template>
