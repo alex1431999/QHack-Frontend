bf9d46f8a4bf09206e0cf84cccbc0afa07f8c019
alex1431999 <alexhaller99@yahoo.de>
2025-04-23 21:18:13 +0200

    feat: add score

--- src/components/EvaluationScore.vue
+++ src/components/EvaluationScore.vue
@@ -2,6 +2,7 @@
 import {CATEGORIES, type Category, categoryMapping, type ScoringItemType} from "@/utils/scoring.ts";
 import ScoringCategory from "@/components/ScoringCategory.vue";
 import type {InvestmentStage} from "@/types/evaluation.ts";
+import {computed} from "vue";
 
 const { scoringItems, investmentStage } = defineProps<{ scoringItems: ScoringItemType[], investmentStage: InvestmentStage[] }>()
 
@@ -13,11 +14,20 @@ function getItems(category: Category): ScoringItemType[] {
   if (!category) return []
   return scoringItems.filter((item: ScoringItemType) => getCategory(item) === category)
 }
+
+const score = 0.73 // TODO
+const scoreThreshold = 0.7
+const scoreColor: string = computed(() => score > scoreThreshold ? 'success': 'error')
 </script>
 
 <template>
-  <v-card class="overflow-auto" width="500" height="400" elevation="10">
-    <v-card-title>Score</v-card-title>
+  <v-card :color="scoreColor" class="overflow-auto" width="500" height="400" elevation="10">
+    <v-card-title>
+      <div class="d-flex justify-space-between">
+        <div>Score</div>
+        <div>{{ score }}</div>
+      </div>
+    </v-card-title>
     <v-card-text>
       <v-expansion-panels>
         <ScoringCategory
