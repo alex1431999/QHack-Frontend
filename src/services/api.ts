2d65c60d3e9ca78c949837cc4b3182ec39e7a0f6
alex1431999 <alexhaller99@yahoo.de>
2025-04-23 14:58:13 +0200

    feat: implement actual request

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,16 @@
 import {sleep} from "@/utils/sleep.ts";
+import axios from "axios";
 
 // TODO define once we know what the evaluation looks like
 export type PitchDeckEvaluation = any
 
 export class ApiService {
+  public static BASE_URL = 'localhost:8000'
+
   public static async uploadPitchDeck(file: File): Promise<PitchDeckEvaluation> {
-    await sleep(3000);
-    return {}
+    const url = `${ApiService.BASE_URL}/upload-pdf/`
+    const response = await axios.post(url, file)
+
+    return response.data
   }
 }
