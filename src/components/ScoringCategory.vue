0efacdb2b0a5691ea3e8ed469fd20e808a022c69
alex1431999 <alexhaller99@yahoo.de>
2025-04-24 09:38:14 +0200

    fix: filter items that don't have a value

--- src/components/ScoringCategory.vue
+++ src/components/ScoringCategory.vue
@@ -4,12 +4,13 @@ import ScoringItem from "@/components/ScoringItem.vue";
 import type {InvestmentStage} from "@/types/evaluation.ts";
 
 const { category, items, investmentStage } = defineProps<{ category: Category, items: ScoringItemType[], investmentStage: InvestmentStage }>()
+const itemsFiltered = items.filter(item => item.value !== undefined)
 </script>
 
 <template>
   <v-expansion-panel :title="category">
     <v-expansion-panel-text>
-      <ScoringItem v-for="item in items" v-if="items.length" :criteria="item.criteria" :value="item.value" :investment-stage="investmentStage" :key="item.criteria" />
+      <ScoringItem v-for="item in items" v-if="itemsFiltered.length" :criteria="item.criteria" :value="item.value" :investment-stage="investmentStage" :key="item.criteria" />
       <div v-else>No data found...</div>
     </v-expansion-panel-text>
   </v-expansion-panel>
